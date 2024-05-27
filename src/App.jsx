13621b21c7229c7cb3933f2831cc47405607ad66
viviansongg <88507197+viviansongg@users.noreply.github.com>
2024-05-27 19:21:02 -0400

    changed to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components/components';
 
 
@@ -6,7 +6,7 @@ const App = () => {
 
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <div className="relative z-0 bg-primary">
           <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
             <Navbar />
@@ -22,7 +22,7 @@ const App = () => {
             <StarsCanvas />
           </div>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
