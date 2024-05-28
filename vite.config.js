03fd73e98e33a79224837e0b040d80929f27e22a
viviansongg <88507197+viviansongg@users.noreply.github.com>
2024-05-28 14:12:48 -0400

    changed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/viviansongg.github.io/",
+  base: "/",
+  // base: "/viviansongg.github.io/",
 })
